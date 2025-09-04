b2dc34ed5f76dc75aae1d9cc951689c6894fa64d
ozanzadeoglu <ozanzadeoglu01@gmail.com>
2025-09-04 13:26:30 +0300

    Add new queries, that are aligned with current objects.
    restaurantSummariesQuery, is used for fetching RestaurantSummaryModel objects, this objects will be used in AllRestaurants tab,
    
    restaurantDetailQuery, is used for fetching RestaurantModel objects, this objects will be used in restaurants detail view screens.
    
    restaurantSummariesWithIdsQuery, is used for fetching RestaurantSummaryModel objects by restaurant id's. This will be used to fetch favorited restaurants on favorites tab.

--- lib/query.dart
+++ lib/query.dart
@@ -1,23 +1,12 @@
-String query(int offset) => '''
-  query getRestaurants {
-    search(location: "Las Vegas", limit: 1, offset: $offset) {
-      total    
+String restaurantSummariesQuery({int limit = 1, int offset = 0}) => '''
+  query getRestaurantSummaries {
+    search(location: "Las Vegas", limit: $limit, offset: $offset) {
       business {
         id
         name
         price
         rating
         photos
-        reviews {
-          id
-          rating
-          text
-          user {
-            id
-            image_url
-            name
-          }
-        }
         categories {
           title
           alias
@@ -25,9 +14,59 @@ String query(int offset) => '''
         hours {
           is_open_now
         }
-        location {
-          formatted_address
+      }
+    }
+  }
+  ''';
+
+String restaurantSummariesWithIdsQuery(List<String> ids) => '''
+  query getRestaurantSummariesWithIds {
+    ${ids.asMap().entries.map((entry) => '''
+    business${entry.key}: business(id: "${entry.value}") {
+      id
+      name
+      price
+      rating
+      photos
+      categories {
+        title
+        alias
+      }
+      hours {
+        is_open_now
+      }
+    }
+    ''',).join('\n')},
+  }
+  ''';
+
+String restaurantDetailQuery(String id) => '''
+  query getRestaurantDetail {
+    business(id: "$id") {
+      id
+      name
+      price
+      rating
+      photos
+      reviews {
+        id
+        rating
+        text
+        user {
+          id
+          image_url
+          name
         }
+      }
+      categories {
+        title
+        alias
+      }
+      hours {
+        is_open_now
+      }
+      location {
+        formatted_address
       }
     }
   }
